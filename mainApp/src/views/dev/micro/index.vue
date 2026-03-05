b498abb425e7daaf607e61cb0b4563071ef75360
shisong <rgkjhshi@gmail.com>
2026-03-05 19:05:26 +0800

    Update index.vue

--- mainApp/src/views/dev/micro/index.vue
+++ mainApp/src/views/dev/micro/index.vue
@@ -7,21 +7,18 @@
 <script setup>
 import { useRoute } from "vue-router";
 import { watch } from "vue";
-import { startQiankun } from "@/microApp.ts";
+import { microMap, startQiankun } from "@/microApp.ts";
 
 // 获取当前路由对象（route 是响应式的，路由变化时会自动更新）
 const route = useRoute()
+const subAppUrl = ref(null);
+let subAppName = ref("subApp");
 
-// 首次加载会调用onMounted但route不会改变
+
 onMounted(() => {
-  console.log('AppMain#microApp onMounted...')
-  // 确保启动Garfish时挂载点已经就绪
+  console.log('主应用中的挂载容器#microApp已就绪...')
+  // 确保启动Qiankun时挂载点已经就绪
   startQiankun()
-})
-
-// 非首次加载则不再调用onMounted，但route会改变
-watch(route, (to) => {
-  console.log('AppMain#microApp onWatch...')
 })
 
 </script>
