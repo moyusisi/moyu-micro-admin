d1a2b0e0e573f4689516317582e4e98cba2a2080
shisong <rgkjhshi@gmail.com>
2026-03-05 18:58:05 +0800

    Update microApp.ts

--- mainApp/src/microApp.ts
+++ mainApp/src/microApp.ts
@@ -5,6 +5,12 @@ console.log('===== 打印 import.meta.env =====', import.meta.env)
 console.log('当前环境模式：', import.meta.env.MODE)
 console.log('是否为开发环境：', import.meta.env.DEV)
 
+// 子应用的激活路径(<name,activePath>)映射
+export const microMap: Record<string, string> = {
+  "subApp1": "/subApp1",
+  "subApp2": "/subApp2",
+};
+
 // 注册微应用列表
 registerMicroApps([
   {
@@ -14,12 +20,30 @@ registerMicroApps([
     // - 开发环境：子应用的 Vite 开发服务器地址（需先启动子应用）
     // - 生产环境：子应用打包后的静态资源地址（如 CDN 或主应用的子路径）
     // entry: '//localhost:82/',
+    entry: import.meta.env.MODE === 'dev' ? 'http://82.157.187.160:82/' : 'http://82.157.187.160:82/',
+    // 主应用中挂载子应用的容器 DOM 节点（需在主应用模板中定义）
+    container: '#microApp',
+    // 子应用的激活规则：当主应用路由匹配到该路径时，自动挂载子应用
+    activeRule: microMap['subApp1'],
+    // 可选：主应用传递给子应用的数据（子应用在 mount 生命周期方法中接收）
+    props: {
+      token: localStorage.getItem('TOKEN'),
+      userInfo: JSON.parse(<string>localStorage.getItem('USER_INFO')),
+    },
+  },
+  {
+    // 子应用唯一标识：必须与子应用 vite.config.js 中定义的 appName 一致
+    name: 'subApp2',
+    // 子应用入口地址：
+    // - 开发环境：子应用的 Vite 开发服务器地址（需先启动子应用）
+    // - 生产环境：子应用打包后的静态资源地址（如 CDN 或主应用的子路径）
+    // entry: '//localhost:82/',
     entry: import.meta.env.MODE === 'dev' ? '//localhost:82/' : 'http://82.157.187.160:82/',
     // 主应用中挂载子应用的容器 DOM 节点（需在主应用模板中定义）
     container: '#microApp',
     // 子应用的激活规则：当主应用路由匹配到该路径时，自动挂载子应用
-    activeRule: '/subApp1',
-    // 可选：传递给子应用的自定义参数（子应用在 mount 生命周期中可接收）
+    activeRule: microMap['subApp2'],
+    // 可选：主应用传递给子应用的数据（子应用在 mount 生命周期方法中接收）
     props: {
       token: localStorage.getItem('TOKEN'),
       userInfo: JSON.parse(<string>localStorage.getItem('USER_INFO')),
