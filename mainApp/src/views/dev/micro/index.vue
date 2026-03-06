3789c906942473a8fb13adb3e0fd973e91dcc725
shisong <rgkjhshi@gmail.com>
2026-03-06 17:32:05 +0800

    Update index.vue

--- mainApp/src/views/dev/micro/index.vue
+++ mainApp/src/views/dev/micro/index.vue
@@ -2,7 +2,7 @@
   <div class="admin-ui-main">
     <a-spin :spinning="subLoading" tip="子应用加载中..." size="large" v-if="subLoading" style="background-color: #f6f8f9" />
     <!--保活模式，name相同则复用一个子应用实例，改变url无效，必须采用通信的方式告知路由变化 -->
-    <WujieVue width="100%" height="100%" :name="subAppName" :url="subAppUrl" :sync="false"
+    <WujieVue width="100%" height="100%" :name="subAppName" :url="subAppUrl" :props="props" :sync="false"
               :beforeLoad="beforeLoad" :beforeMount="beforeMount"></WujieVue>
   </div>
 </template>
@@ -14,10 +14,8 @@ import { microMap } from "@/microApp.ts";
 
 // 获取当前路由对象（route 是响应式的，路由变化时会自动更新）
 const route = useRoute()
-
 // 子应用loading
 const subLoading = ref(false);
-
 // 子应用所需参数
 const subAppUrl = ref(null);
 let subAppName = ref("subApp");
